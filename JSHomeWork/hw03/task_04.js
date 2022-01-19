cdcb1b4e19b3c98c2a395357c40dfca8dca5acf1
Aleksej Sharin <aleksej.sharin@privatbank.ua>
2022-01-20 00:05:25 +0200

    hw03 - function

---
+++ JSHomeWork/hw03/task_04.js
@@ -0,0 +1,51 @@
+/*Задача 4
+Сделайте функцию `f`, которая принимает параметром число от 1 до 7,
+а затем возвращает день недели на русском языке.
+Данная функция должна обязательно содержать проверку входного параметра, потому что принимать она может только числа от 1 до 7.
+
+f(1); // Воскресенье
+f(2); // Понедельник
+f(8); // Error: parameter should be in the range of 1 to 7
+f('1'); // Error: parameter type is not a Number
+*/
+//=====================================================
+function f(a) {
+  if (typeof a === "number") {
+    if (a >= 1 && a <= 7) {
+      switch (a) {
+        case 1:
+          msg = "Воскресенье";
+          break;
+        case 2:
+          msg = "Понедельник";
+          break;
+        case 3:
+          msg = "Вторник";
+        case 4:
+          msg = "Среда";
+          break;
+        case 5:
+          msg = "Чертверг";
+          break;
+        case 6:
+          msg = "Пятница";
+          break;
+        case 7:
+          msg = "Суббота";
+          break;
+      }
+      console.log(msg);
+    } else {
+      throw new Error("parameter should be in the range of 1 to 7"); //выбрасываем ошибку
+    }
+  } else {
+    throw new Error("parameter type is not a Number"); //выбрасываем ошибку
+  }
+}
+//=====================================================
+f(1);   // Воскресенье
+f(2);   // Понедельник
+f(7);   // Суббота
+f(8);   // Error: parameter should be in the range of 1 to 7
+f('1'); // Error: parameter type is not a Number
+//=====================================================
---
+++ JSHomeWork/hw03/task_06.js
@@ -0,0 +1,27 @@
+/*Задача 6
+Сделайте функцию `isEven()`,
+которая параметром принимает целое число и проверяет: чётное оно или нет.
+Если чётное — функция возвращает `true`, если нечётное — `false`.
+Данная функция должна обязательно содержать проверку входного параметра, потому что принимать она может только число.
+
+*Чётные числа могут делится на 2 без остатка.*
+
+isEven(3); // false
+isEven(4); // true
+isEven('Content'); // Error: parameter type is not a Number
+*/
+//=====================================================
+function isEven(a){
+    if (typeof a === "number") {
+        let res = (a % 2 == 0) ? true : false;
+        // return res;
+        console.log(res);
+      } else {
+        throw new Error("parameter type is not a Number"); //выбрасываем ошибку
+      }
+}
+//=====================================================
+isEven(3);         // false
+isEven(4);         // true
+isEven('Content'); // Error: parameter type is not a Number
+//=====================================================
